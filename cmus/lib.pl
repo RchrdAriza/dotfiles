dc45866063d1b16024d89d90a6767ec5c2799420
RchrdAriza <rchrdalv@gmail.com>
2024-03-14 14:06:01 -0500

    first commit

---
+++ cmus/lib.pl
@@ -0,0 +1,103 @@
+/storage/emulated/0/Download/Spotube/No Les Creo - 1915.m4a
+/storage/emulated/0/Download/Spotube/A Prophecy - Asking Alexandria.m4a
+/storage/emulated/0/Download/Spotube/Just Pretend - Bad Omens.m4a
+/storage/emulated/0/Download/Spotube/IDWT$ - Bad Omens.m4a
+/storage/emulated/0/Download/Spotube/Sin Vos No Puedo - Bandalos Chinos.m4a
+/storage/emulated/0/Download/Spotube/In My Head - Bedroom.m4a
+/storage/emulated/0/Download/Spotube/Children of the Grave (2009 - Remaster) - Black Sabbath.m4a
+/storage/emulated/0/Download/Spotube/Baby I'm Yours - Breakbot, Irfane.m4a
+/storage/emulated/0/Download/Spotube/Teardrops - Bring Me The Horizon.m4a
+/storage/emulated/0/Download/Spotube/Kool-Aid - Bring Me The Horizon.m4a
+/storage/emulated/0/Download/Spotube/One Day The Only Butterflies Left Will Be In Your Chest As You March Towards Your Death (feat. Amy Lee) - Bring Me The Horizon, Amy Lee.m4a
+/storage/emulated/0/Download/Spotube/nihilist blues (feat. Grimes) - Bring Me The Horizon, Grimes.m4a
+/storage/emulated/0/Download/Spotube/Obey (with YUNGBLUD) - Bring Me The Horizon, YUNGBLUD.m4a
+/storage/emulated/0/Download/Spotube/Waking the Demon - Bullet For My Valentine.m4a
+/storage/emulated/0/Download/Spotube/Duvet - bôa.m4a
+/storage/emulated/0/Download/Spotube/Outro - C.R.O.m4a
+/storage/emulated/0/Download/Spotube/Ruinas - C.R.O.m4a
+/storage/emulated/0/Download/Spotube/Ciudad Gris - C.R.O.m4a
+/storage/emulated/0/Download/Spotube/Eres - Café Tacvba.m4a
+/storage/emulated/0/Download/Spotube/Redbone - Childish Gambino.m4a
+/storage/emulated/0/Download/Spotube/Cabildo y Juramento - Conociendo Rusia.m4a
+/storage/emulated/0/Download/Spotube/Otra Oportunidad - Conociendo Rusia.m4a
+/storage/emulated/0/Download/Spotube/Piel Canela - Cuco.m4a
+/storage/emulated/0/Download/Spotube/Digital Bath - Deftones.m4a
+/storage/emulated/0/Download/Spotube/Hay Que Abandonar Este Lugar - Depresión Sonora.m4a
+/storage/emulated/0/Download/Spotube/Dime Que Me Odias - Depresión Sonora.m4a
+/storage/emulated/0/Download/Spotube/seguiremos siendo - Depresión Sonora, Bratty.m4a
+/storage/emulated/0/Download/Spotube/Respirar. - Distimia Agorafóbica.m4a
+/storage/emulated/0/Download/Spotube/El Mar - Distimia Agorafóbica.m4a
+/storage/emulated/0/Download/Spotube/Señor de la Noche - Don Omar.m4a
+/storage/emulated/0/Download/Spotube/Where'd All the Time Go  - Dr. Dog.m4a
+/storage/emulated/0/Download/Spotube/Flores - DRIMS.m4a
+/storage/emulated/0/Download/Spotube/Mon esprit part en couilles - Expression Direkt.m4a
+/storage/emulated/0/Download/Spotube/Crazy - Fazzini, Franky Style.m4a
+/storage/emulated/0/Download/Spotube/Colorama - Gativideo.m4a
+/storage/emulated/0/Download/Spotube/Marta tiene un marcapasos - Hombres G.m4a
+/storage/emulated/0/Download/Spotube/Te quiero - Hombres G.m4a
+/storage/emulated/0/Download/Spotube/Shut up My Moms Calling - Hotel Ugly.m4a
+/storage/emulated/0/Download/Spotube/Warm Memory - Human Tetris.m4a
+/storage/emulated/0/Download/Spotube/Decapitation Fornication - Infant Annihilator.m4a
+/storage/emulated/0/Download/Spotube/Despacio - Isla de Caras.m4a
+/storage/emulated/0/Download/Spotube/no le mienta a mamá - Jaze.m4a
+/storage/emulated/0/Download/Spotube/Eres para Mí (with Anita Tijoux) - Julieta Venegas, Ana Tijoux.m4a
+/storage/emulated/0/Download/Spotube/Stronger - Kanye West.m4a
+/storage/emulated/0/Download/Spotube/I Wonder - Kanye West.m4a
+/storage/emulated/0/Download/Spotube/Heartless - Kanye West.m4a
+/storage/emulated/0/Download/Spotube/Bound 2 - Kanye West.m4a
+/storage/emulated/0/Download/Spotube/Famous - Kanye West.m4a
+/storage/emulated/0/Download/Spotube/Hurricane - Kanye West.m4a
+/storage/emulated/0/Download/Spotube/Everything I Am - Kanye West, DJ Premier.m4a
+/storage/emulated/0/Download/Spotube/Flashing Lights - Kanye West, Dwele.m4a
+/storage/emulated/0/Download/Spotube/A.D.I.D.A.S. - Korn.m4a
+/storage/emulated/0/Download/Spotube/Bésame - La Texana.m4a
+/storage/emulated/0/Download/Spotube/Nunca he sabido amar - La Texana.m4a
+/storage/emulated/0/Download/Spotube/Atrapado - La Texana.m4a
+/storage/emulated/0/Download/Spotube/Un día como hoy - La Texana.m4a
+/storage/emulated/0/Download/Spotube/Accidente - Las Ligas Menores.m4a
+/storage/emulated/0/Download/Spotube/Solo - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Ha Sido - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Ya No Te Quiero - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Amor Prohibido II - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Culero - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Punta Diamante - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Playa Privada - Los Blenders.m4a
+/storage/emulated/0/Download/Spotube/Por el Resto - Los Enanitos Verdes.m4a
+/storage/emulated/0/Download/Spotube/Amores Lejanos - Los Enanitos Verdes.m4a
+/storage/emulated/0/Download/Spotube/Ahora Te Puedes Marchar - Luis Miguel.m4a
+/storage/emulated/0/Download/Spotube/Oye Mi Amor - Maná.m4a
+/storage/emulated/0/Download/Spotube/Alv Bye - Melted Ice Cream.m4a
+/storage/emulated/0/Download/Spotube/Show Me How - Men I Trust.m4a
+/storage/emulated/0/Download/Spotube/Rapp Snitch Knishes feat. Mr. Fantastik - MF DOOM.m4a
+/storage/emulated/0/Download/Spotube/Desaparecer - Mundaka.m4a
+/storage/emulated/0/Download/Spotube/Famous Last Words - My Chemical Romance.m4a
+/storage/emulated/0/Download/Spotube/Demonios Falle Otra Vez - Nico Nico.m4a
+/storage/emulated/0/Download/Spotube/Mi Amor es el Mar - O Tortuga.m4a
+/storage/emulated/0/Download/Spotube/Television Romance - Pale Waves.m4a
+/storage/emulated/0/Download/Spotube/Pensamientos Nocturnos - Peces Raros.m4a
+/storage/emulated/0/Download/Spotube/La Playa - Mambo - Perras on the beach.m4a
+/storage/emulated/0/Download/Spotube/Tuca - Perras on the beach.m4a
+/storage/emulated/0/Download/Spotube/Disculpa los Malos Pensamientos - PXNDX.m4a
+/storage/emulated/0/Download/Spotube/3+1 - PXNDX.m4a
+/storage/emulated/0/Download/Spotube/Another One Bites The Dust - Queen.m4a
+/storage/emulated/0/Download/Spotube/Yo Soy Lo Peor Que Te Ha Pasado En La Vida - Rojuu, Carzé, Travy Dev.m4a
+/storage/emulated/0/Download/Spotube/Mi Luz (ft. Rels B) - Rvfv, Rels B.m4a
+/storage/emulated/0/Download/Spotube/Siempre Te Admiré - San Charbel.m4a
+/storage/emulated/0/Download/Spotube/Verde Pastel - Señor Kino.m4a
+/storage/emulated/0/Download/Spotube/Sandwich de Monda - Sgt. Papers.m4a
+/storage/emulated/0/Download/Spotube/Se Siente Bien - Sgt. Papers.m4a
+/storage/emulated/0/Download/Spotube/Sos Todo Lo Que Está Bien - Silvestre y La Naranja.m4a
+/storage/emulated/0/Download/Spotube/Amores Callejeros - Silvestre y La Naranja.m4a
+/storage/emulated/0/Download/Spotube/Wait and Bleed - Slipknot.m4a
+/storage/emulated/0/Download/Spotube/Cuando Pase El Temblor - Remasterizado 2007 - Soda Stereo.m4a
+/storage/emulated/0/Download/Spotube/Some - Steve Lacy.m4a
+/storage/emulated/0/Download/Spotube/Te Miro Para Ver Si Me Ves Mirarte - Surfistas Del Sistema.m4a
+/storage/emulated/0/Download/Spotube/te odio - te vi en un planetario.m4a
+/storage/emulated/0/Download/Spotube/Talking To Myself - The Linda Lindas.m4a
+/storage/emulated/0/Download/Spotube/Cariño - The Marías.m4a
+/storage/emulated/0/Download/Spotube/You Always Hurt The One You Love - The Mills Brothers.m4a
+/storage/emulated/0/Download/Spotube/Upside Down - The Story So Far.m4a
+/storage/emulated/0/Download/Spotube/The Adults Are Talking - The Strokes.m4a
+/storage/emulated/0/Download/Spotube/Not Allowed - TV Girl.m4a
+/storage/emulated/0/Download/Spotube/One Minute (feat. Kanye West & Travis Barker) - XXXTENTACION, Kanye West, Travis Barker.m4a
+/storage/emulated/0/Download/Spotube/Azul - Zoé.m4a
---
+++ helix/README.md
@@ -0,0 +1,15 @@
+# Helix config files
+
+[Helix](https://github.com/helix-editor/helix) is a vim-like editor,
+but a lot more beginner friendly. I've tried to switch to neovim multiple
+times (more than I'd like to admit), but I never actually did it.
+
+I've randomly found Helix and now using it full time, I'd advice you to give
+it a try, especially if you think that terminal/modal editing is cool,
+but the (neo)vim itself is not
+
+Also, thanks for mentioning me in the [awesome-helix](https://github.com/rajasegar/awesome-helix) repo 🎉
+
+## Preview
+
+![image](https://github.com/ravsii/.helix/assets/5007271/8ad53599-cf50-4597-a1f5-0fd88d513ad7)
