3b68989517280d33c602330452414d776881bd2c
RchrdAriza <rchrdalv@gmail.com>
2025-12-27 13:45:46 -0500

    cambié algunos iconos

--- fastfetch/fastfetch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-LOGO_DIR="$HOME/.config/fastfetch/logo"
-if [ -d "$LOGO_DIR" ] && command -v kitty >/dev/null 2>&1; then
-    RANDOM_LOGO=$(find "$LOGO_DIR" -type f -name "*logo*" \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.webp" \) | shuf -n 1)
