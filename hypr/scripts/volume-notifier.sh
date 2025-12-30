172f8928c912e43faf91570fec3dfde87c2dbf23
RchrdAriza <rchrdalv@gmail.com>
2025-12-30 15:15:07 -0500

    add volume notifier script

---
+++ hypr/scripts/volume-notifier.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ID=9868
+
+VOLUME_LEVEL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}')
+
+# echo $VOLUME_LEVEL
+notify-send \
+  -a "volume-level" \
+  -r "$ID" \
+  "Volume" \
+  "$VOLUME_LEVEL"
